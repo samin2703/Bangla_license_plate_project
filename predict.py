40f83f1f24a7dcc7fec0c521b3c7c81d930d0951
Samin Sadique Aurin <121294949+samin2703@users.noreply.github.com>
2025-12-16 01:13:26 +0600

    Update predict.py

--- predict.py
+++ predict.py
@@ -4,9 +4,10 @@ import cv2
 model = YOLO("best.pt")  
 
 results = model.predict(source="test_image.jpg", save=True, conf=0.25)
-
+# predict
 for r in results:
     annotated_frame = r.plot() 
     cv2.imshow("Prediction", annotated_frame)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+
